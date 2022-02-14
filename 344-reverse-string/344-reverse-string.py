feb3f8ae102ebc36c1b5897a414577deb95e038c
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-02-13 22:29:49 -0500

    Time: 356 ms (18.95%), Space: 18.4 MB (74.90%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -6,6 +6,7 @@ class Solution:
         l, r = 0, len(s)-1
         
         while l < r:
+            #swap using ',' operator 
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
