18d9e83b584daf2944573824e097f7966925f741
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-03-03 17:38:26 -0500

    Time: 68 ms (55.10%), Space: 15.2 MB (9.35%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,27 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # Two methods - Sorting(O(NlogN)) and Using HAshmap(O(N))
-        if len(s) != len(t):
-            return False
-        d1 = {}
-        d2 = {}
+#         if len(s) != len(t):
+#             return False
+#         d1 = {}
+#         d2 = {}
         
-        for c in s:
-            if c in d1:
-                d1[c] += 1
-            else:
-                d1[c] = 1
-        for c in t:
-            if c in d2:
-                d2[c] += 1
-            else:
-                d2[c] = 1
-        for c in d1 :
-            if( c not in d2):
-                return False
-            elif d1[c] == d2[c]:
-                continue
-            else:#count of char not same
-                return False
-        return True
+#         for c in s:
+#             if c in d1:
+#                 d1[c] += 1
+#             else:
+#                 d1[c] = 1
+#         for c in t:
+#             if c in d2:
+#                 d2[c] += 1
+#             else:
+#                 d2[c] = 1
+#         for c in d1 :
+#             if( c not in d2):
+#                 return False
+#             elif d1[c] == d2[c]:
+#                 continue
+#             else:#count of char not same
+#                 return False
+#         return True
         
+        s = sorted(s)
+        t = sorted(t)
+        return s == t
