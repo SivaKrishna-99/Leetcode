458d991bf1b13c737aa031ddfb022e640b202c92
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-03-04 17:17:56 -0500

    Time: 66 ms (63.30%), Space: 18 MB (53.46%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -5,20 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    count = 0
+    num = float('inf')
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
-        res = []
-        self.helper(root,res)
-        for i in range(0,k):
-            if i == k-1:
-                return res[k-1]
-        
+        # res = []
+        if not root:
+            return None
+        self.helper(root,k)
+        # for i in range(0,k):
+        #     if i == k-1:
+        #         return res[k-1]
+        return self.num
     
-    def helper(self,root,res):
+    def helper(self,root,k):
+        if not root :
+            return self.count
         
-        if root:
-            self.helper(root.left,res)
-            res.append(root.val)
-            self.helper(root.right,res)
+        self.helper(root.left,k)
+        self.count = self.count+1
+        if self.count == k:
+            self.num = root.val
+        # res.append(root.val)
+        self.helper(root.right,k)
+       
+        return self.count
             
             
