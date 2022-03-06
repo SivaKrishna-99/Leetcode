2a9565de81505e6828f4b222b1a2965046731a09
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-03-06 18:04:19 -0500

    Time: 5104 ms (90.31%), Space: 14 MB (43.51%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        m = len(board)
+        n = len(board[0])
+        
+        for i in range(0,m):
+            for j in range(0,n):
+                if self.dfs(i,j,m,n,board,0,word):
+                    return True
+        return False
+                               
+                    
+    def dfs(self,i,j,m,n,board,k,word):
+        if k == len(word):
+            return True
+        if i < 0  or j < 0 or i >= m or j >= n or board[i][j] != word[k]:
+            return False
+        
+        temp = board[i][j]
+        board[i][j] = "0"
+        res = (self.dfs(i+1,j,m,n,board,k+1,word) or
+        self.dfs(i-1,j,m,n,board,k+1,word) or 
+        self.dfs(i,j+1,m,n,board,k+1,word) or
+        self.dfs(i,j-1,m,n,board,k+1,word) )
+        board[i][j] = temp
+        return res
+                    
+                    
