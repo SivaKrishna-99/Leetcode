faea2cd5551bb923b19a76cd907f7ce168db6d5d
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-04-07 15:43:02 -0400

    Time: 48 ms (87.78%), Space: 14.4 MB (88.16%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -2,25 +2,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        # s = re.sub('\W+','', s )
-        #'.join(e for e in string if e.isalnum())
-        #re.sub('[^A-Za-z0-9]+', '', string) - of spaces is needed instaed of " use ''
-        #re.sub('\W+','', s ) - better one 
+       
+        # s1 = ""
         
-        # s = ''.join((c.lower()) for c in s if c.isalnum())
-        s1 = ""
+#         for c in s:
+#             if c.isalnum():
+#                 s1 += c.lower()
         
-        for c in s:
-            if c.isalnum():
-                s1 += c.lower()
+#         return s1 == s1[::-1]    
         
-        return s1 == s1[::-1]    
+        #better way is to use Two pointers technique
+        left ,right = 0, len(s)-1
         
-        # rev = ''.join(reversed(s))
-        #s == s[::-1]
-        # run the for loop for len/2 and comapre values 
-        #can use a flag and then start comparing from 0 to -1 and increment
-        #s==s[::-1]
+        while left < right:
+            
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        return True
+       
         
         
         
