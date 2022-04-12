8ff1393ed4aaf76aa100d1b1f84c68f4d816d923
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-04-12 01:25:47 -0400

    Time: 72 ms (56.99%), Space: 14.1 MB (42.13%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.py
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -1,23 +1,38 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
         
-        #no of L's and R's equal and U and D's 
-        l,r,u,d = 0,0,0,0
-        print("Hello")
+        # #no of L's and R's equal and U and D's 
+        # l,r,u,d = 0,0,0,0
+        # for i in range(len(moves)):
+        #     if moves[i] == 'L':
+        #         l += 1
+        #     elif(moves[i] == 'R'):
+        #         r += 1
+        #     elif(moves[i] == 'U'):
+        #         u += 1
+        #         print(u)
+        #     else:
+        #         d += 1
+        #         print(d)
+        # if l == r and u ==d :
+        #     return True
+        # else:
+        #     return False
+        
+        xSum = 0
+        ySum = 0
         for i in range(len(moves)):
-            print("is it entering the loop")
             if moves[i] == 'L':
-                l += 1
+                xSum -= 1
             elif(moves[i] == 'R'):
-                r += 1
+                xSum += 1
             elif(moves[i] == 'U'):
-                u += 1
-                print(u)
+                ySum += 1
             else:
-                d += 1
-                print(d)
-        if l == r and u ==d :
+                ySum -= 1
+        if xSum ==0 and ySum == 0:
             return True
         else:
             return False
+        
         
