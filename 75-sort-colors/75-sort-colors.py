5f00eec82109bc1ee015ab36284071d28a9ce487
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-04-16 23:06:37 -0400

    Time: 30 ms (94.86%), Space: 13.9 MB (67.09%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,28 +3,49 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        freq = [0]*3
+        freq0,freq1,freq2 = 0,0,0
         for i in range(len(nums)):
             if nums[i] == 0:
-                freq[0] += 1
+                freq0 += 1
             elif(nums[i] == 1):
-                freq[1] += 1
+                freq1 += 1
             else:
-                freq[2] += 1
-        
+                freq2 += 1
         i = 0
-        while freq[0] >0:
+        while freq0 > 0:
             nums[i] = 0
             i += 1
-            freq[0] -= 1
-        while freq[1]>0:
+            freq0 -= 1
+        while freq1>0:
             nums[i] = 1
             i += 1
-            freq[1] -= 1
-        while freq[2] > 0:
+            freq1 -= 1
+        while freq2 > 0:
             nums[i] = 2
             i += 1
-            freq[2] -= 1
+            freq2 -= 1
+#         freq = [0]*3
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 freq[0] += 1
+#             elif(nums[i] == 1):
+#                 freq[1] += 1
+#             else:
+#                 freq[2] += 1
+        
+#         i = 0
+#         while freq[0] >0:
+#             nums[i] = 0
+#             i += 1
+#             freq[0] -= 1
+#         while freq[1]>0:
+#             nums[i] = 1
+#             i += 1
+#             freq[1] -= 1
+#         while freq[2] > 0:
+#             nums[i] = 2
+#             i += 1
+#             freq[2] -= 1
             
      
         
