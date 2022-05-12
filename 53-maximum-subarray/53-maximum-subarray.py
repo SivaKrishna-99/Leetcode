d63b09dc705dc40b6b0d4a539b264af94597d7d5
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-05-12 18:27:52 -0400

    Time: 839 ms (65.73%), Space: 27.9 MB (79.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         #Kadane's Algorithm
-        b = float('-inf')
-        c = 0
+        maxSum = float('-inf')
+        currSum = 0
         for num in nums:
-            c = max(num, c+num)
-            b = max(b, c) 
-        return b
+            currSum = max(num, currSum+num)
+            maxSum = max(maxSum, currSum) 
+        return maxSum
