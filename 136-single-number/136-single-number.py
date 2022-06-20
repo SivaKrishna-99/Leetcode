22357ae435a756b18aa62524fa96122244799ead
SivaKrishna <81541894+SivaKrishna-99@users.noreply.github.com>
2022-06-20 11:39:09 -0400

    Time: 263 ms (24.52%), Space: 16.6 MB (93.58%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        temp = 0
-        for i in range ( len(nums) ):
-            temp ^= nums[i]
-            
-        return temp
-        
+        #XOR
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
